dced09243e98ad245099d4313286ab2480381434
Ethan <donahue.ethanj@gmail.com>
2023-05-09 21:24:59 -0400

    Add basic weather

--- weather.py
+++ weather.py
@@ -1,2 +1,15 @@
-from bs4 import BeautifulSoup
+from config import api_key
+import requests
 
+location = input("Enter location: ")
+
+url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={location}"
+
+response = requests.get(url)
+print(response.json())
+
+temperature_c = response.json()['current']['temp_c']  # Temperature in Celsius
+temperature_f = response.json()['current']['temp_f']  # Temperature in Fahrenheit
+
+# Print the temperature in Celsius and Fahrenheit
+print(f'Temperature in {location}: {temperature_c}°C ({temperature_f}°F)')
