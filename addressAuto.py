b17dc51bb8a8a2a099630b04ef2fd3cb465376ff
Ethan <donahue.ethanj@gmail.com>
2023-05-12 12:59:07 -0400

    Add address auto complete

--- addressAuto.py
+++ addressAuto.py
@@ -1,11 +1,13 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 
-url = "https://api.geoapify.com/v1/geocode/autocomplete?text=Mosco&apiKey=53a7ab2226e84cd48b9bd599eca20ae3"
+text = "Fish"
+
+url = f"https://api.geoapify.com/v1/geocode/autocomplete?text={text}&apiKey=53a7ab2226e84cd48b9bd599eca20ae3"
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
 
 resp = requests.get(url, headers=headers)
 
-print(resp.status_code)
+print(resp.json())
